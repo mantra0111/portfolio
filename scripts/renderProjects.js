62fa0cb5dce5916e4adcc07985e0524f9f180f48
Mateo Sierra <m.sierra1037@hotmail.com>
2021-07-06 16:14:37 -0500

    i don't like these project cards

---
+++ scripts/renderProjects.js
@@ -0,0 +1,42 @@
+const imageUrls = [
+    "./images/dex-min.PNG",
+    "./images/gym-responsive-min.PNG"]
+
+
+// name 
+const projects = [
+    {
+        projectName: "React pokedex",
+        mediaLocation: "./images/dex-min.PNG",
+        sourceCodeLink: "https://github.com/mantra0111/pokedex",
+        demoLink: "https://mantra0111.github.io/pokedex/#/",
+        cardColor: "",
+    },
+    {
+        projectName: "Gym landing page",
+        mediaLocation: "./images/gym-responsive-min.PNG",
+        sourceCodeLink: "https://github.com/mantra0111/gym-landing-page",
+        demoLink: "https://mantra0111.github.io/gym-landing-page/",
+        cardColor: "",
+    },
+]
+
+
+const makeProjectCard = ({ imgUrl, projectName }) => {
+    const cardTemplate = `
+        <li class="project-card">
+            <img class="project-media" src=${imgUrl}>
+            <div class="project-info"> ${projectName} </div>
+        </li>`
+    return cardTemplate
+}
+
+const projectListContainer = document.querySelector('.masonry-gallery')
+
+projects.forEach((project) => {
+    projectListContainer.innerHTML +=
+        makeProjectCard({
+            imgUrl: project.mediaLocation,
+            projectName: project.projectName
+        })
+})
