580f5160b55f5cd8cf24a08e790d1d6722e8ab44
Mateo Sierra <m.sierra1037@hotmail.com>
2021-08-01 15:36:05 -0500

    new hero section and selectors refactor

--- scripts/NavbarLinkHighLight.js
+++ scripts/NavbarLinkHighLight.js
@@ -2,18 +2,20 @@ const splittedUrl = document.location.href.split("/")
 const currentLocation = splittedUrl[splittedUrl.length - 1]
 const homeLink = document.querySelector('#home-link')
 
+let activeLinkStyle = '0.2rem solid var(--active-text-dark)'
+
 switch (currentLocation) {
     case '':
-        homeLink.style.borderBottom = '0.2rem solid crimson';
+        homeLink.style.borderBottom = activeLinkStyle;
         break;
     case 'index.html':
-        homeLink.style.borderBottom = '0.2rem solid crimson';
+        homeLink.style.borderBottom = activeLinkStyle;
         break;
     case 'index.html#projects':
-        homeLink.style.borderBottom = '0.2rem solid crimson';
+        homeLink.style.borderBottom = activeLinkStyle;
         break;
     case '#projects':
-        homeLink.style.borderBottom = '0.2rem solid crimson';
+        homeLink.style.borderBottom = activeLinkStyle;
         break;
 
     default:
