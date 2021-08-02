b7ad8a828afc9c4b5cf4e0166bc405ef1c12bda6
Mateo Sierra <m.sierra1037@hotmail.com>
2021-08-01 20:55:30 -0500

    cv added with better loading screen

--- scripts/loadingSection.js
+++ scripts/loadingSection.js
@@ -4,7 +4,7 @@ function hideLoadingSection() {
     setTimeout(() => {
         //loadingSection.style.transform = "scale(0)";
         loadingSection.style.top = '150vh'
-    }, 1500)
+    }, 800)
 }
 
 document.onload = hideLoadingSection()
